c194cd52333d6c45f4e76dcc7383401e85682e76
ysvln <ysvln0912@gmail.com>
2025-05-08 16:41:40 +0900

    2.3 네비게이팅

--- secion02/src/pages/_app.tsx
+++ secion02/src/pages/_app.tsx
@@ -1,10 +1,30 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import Link from "next/link";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  const onClickButton = () => {
+    router.push("/test");
+
+    // 뒤로가기를 방지하며 페이지 이동
+    // router.replace("/test");
+  };
+
   return (
     <>
-      <header>글로벌 헤더</header>
+      <header>
+        <Link href={"/"}>Index</Link>
+        &nbsp;
+        <Link href={"/search"}>Search</Link>
+        &nbsp;
+        <Link href={"/book/1"}>Book1</Link>
+        <div>
+          <button onClick={onClickButton}>/test 페이지로 이동합니다.</button>
+        </div>
+      </header>
       <Component {...pageProps} />
     </>
   );
