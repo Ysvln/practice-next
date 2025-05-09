dd341c85a33bc0e68505ff86ceb15514124fd932
Ysvln <ysvln0912@gmail.com>
2025-05-09 16:49:22 +0900

    2.12 ssr

--- secion02/src/pages/book/[id].tsx
+++ secion02/src/pages/book/[id].tsx
@@ -1,4 +1,6 @@
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
 import style from "./[id].module.css";
+import fetchOneBook from "../lib/fetch-one-book";
 
 const mockData = {
   id: 1,
@@ -12,16 +14,25 @@ const mockData = {
     "https://shopping-phinf.pstatic.net/main_3888828/38888282618.20230913071643.jpg",
 };
 
-export default function Page() {
-  const {
-    id,
-    title,
-    subTitle,
-    description,
-    author,
-    publisher,
-    coverImgUrl,
-  } = mockData;
+//context 매개변수는 요청 시 서버에서 전달되는 다양한 정보를 담고 있는 객체
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const id = context.params!.id;
+  const book = await fetchOneBook(Number(id));
+
+  return {
+    props: { book },
+  };
+};
+
+export default function Page({
+  book,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  if (!book) return "문제가 발생했습니다. 다시 시도해 주세요.";
+
+  const { id, title, subTitle, description, author, publisher, coverImgUrl } =
+    book;
 
   return (
     <div className={style.container}>
--- secion02/src/pages/index.tsx
+++ secion02/src/pages/index.tsx
@@ -1,49 +1,45 @@
+import { InferGetServerSidePropsType } from "next";
+import { ReactNode } from "react";
 // CSS Module
-import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.css";
-import { ReactNode, useEffect } from "react";
+import SearchableLayout from "@/components/searchable-layout";
 import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
-import { InferGetServerSidePropsType } from "next";
+import fetchBooks from "./lib/fetch-books";
+import fetchRandomBooks from "./lib/fetch-random-books";
 
-export const getServerSideProps = () => {
-  // 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터를 불러오는 함수
-  // 오직 서버 측에서만 발생한다.
-  // 브라우저에서는 확인할 수 없고 터미널에서만 확인 가능하다.
-  console.log("서버사이드 프롭스입니다.");
+export const getServerSideProps = async () => {
+  // 병렬처리
+  const [allBooks, recoBooks] = await Promise.all([
+    fetchBooks(),
+    fetchRandomBooks(),
+  ]);
 
-  const data = "Hello";
   return {
     props: {
-      data,
+      allBooks,
+      recoBooks,
     },
   };
 };
 
 export default function Home({
-  data,
+  allBooks,
+  recoBooks,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  // 아래 코드는 에러가 뜬다. 서버에서 먼저 실행되기 때문에 window가 undefined가 됨.
-  // undefined.location 과 같은 형태로 작성되어 에러가 발생.
-  // window.location
-  console.log(data);
-
-  useEffect(() => {
-    // 마운트 이후 발생하는 함수이기 때문에 서버가 아닌 브라우저에서 동작해서 에러가 발생하지 않음.
-    console.log(window);
-  }, []);
+  console.log(allBooks);
 
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
+        {recoBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
+        {allBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
