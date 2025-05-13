936a66a96a998793bbf60cd86b7b0e7112716812
Ysvln <ysvln0912@gmail.com>
2025-05-13 11:28:12 +0900

    3.7 UI 세팅

--- section03/src/app/(with-searchbar)/search/page.tsx
+++ section03/src/app/(with-searchbar)/search/page.tsx
@@ -1,21 +1,18 @@
-// 서버컴포넌트이기 때문에 async 사용 가능
+import books from "@/mock/books.json";
+import BookItem from "@/components/book-item";
 
-import ClientComponent from "@/components/client-component";
-
-// 서버 측에서 사전 렌더링을 위해 딱 한번 실행 => 비동기적으로 가능
 export default async function Page({
   searchParams,
 }: {
-  searchParams: Promise<{ q: string }>;
+  searchParams: Promise<{ q?: string }>;
 }) {
   const { q } = await searchParams;
+
   return (
-    // client 컴포넌트와 server 컴포넌트를 모두 포함
     <div>
-      Search : {q}
-      <ClientComponent>
-        <></>
-      </ClientComponent>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
     </div>
   );
 }
