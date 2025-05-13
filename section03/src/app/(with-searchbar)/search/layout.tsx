9b237b1400cab7f582b9233029802498d7a55526
Ysvln <ysvln0912@gmail.com>
2025-05-13 11:24:20 +0900

    3.6 네비게이팅

--- section03/src/app/(with-searchbar)/search/layout.tsx
+++ section03/src/app/(with-searchbar)/search/layout.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
-import SearchBar from "./searchBar";
+import SearchBar from "@/components/searchbar";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
